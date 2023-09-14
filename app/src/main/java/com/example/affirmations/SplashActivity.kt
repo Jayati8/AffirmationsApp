faf1a1bc861cc7cb76f7c4141fc27ca2f7f598ac
Jayati Badoni <badonijayati@gmail.com>
2023-09-14 15:07:46 +0530

    snackbar added

--- app/src/main/java/com/example/affirmations/SplashActivity.kt
+++ app/src/main/java/com/example/affirmations/SplashActivity.kt
@@ -4,22 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import android.os.Handler
 import android.os.Looper
 
 @SuppressLint("CustomSplashScreen")
-@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        // This is used to hide the status bar and make the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
 
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
--- app/src/main/java/com/example/affirmations/model/NetworkChangeReceiver.kt
+++ app/src/main/java/com/example/affirmations/model/NetworkChangeReceiver.kt
@@ -1,4 +1,82 @@
 package com.example.affirmations.model
 
-class NetworkChangeReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.view.View
+import com.example.affirmations.MainActivity
+import com.google.android.material.snackbar.Snackbar
+
+@Suppress("DEPRECATION")
+// Define a BroadcastReceiver that listens for network connectivity changes.
+class NetworkChangeReceiver(private val rootView: View, private  val context: MainActivity) : BroadcastReceiver() {
+
+    // This function is called when a broadcast intent is received.
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == ConnectivityManager.CONNECTIVITY_ACTION) {
+            // Get a reference to the ConnectivityManager to check network status.
+            val connectivityManager =
+                context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            // Retrieve information about the currently active network.
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+
+            // Check if the active network is not null and is connected.
+            val isConnected = activeNetworkInfo != null && activeNetworkInfo.isConnected
+            if (isConnected) {
+                // Internet is available, perform your action here
+                showSnackbar("Internet Connected!")
+            }else{
+                // Internet is not available, show the custom Snackbar with a "Retrieve" action
+                showRetrieveSnackbar()
+            }
+        }
+    }
+
+    //A private function to display a snackbar with a specified message
+    private fun showSnackbar(message:String) {
+        // Create a Snackbar with the given message and associate it with the rootView.
+        val snackbar=Snackbar.make(rootView, message, Snackbar.LENGTH_LONG)
+        //show snackbar on screen
+        snackbar.show()
+    }
+
+    private fun showRetrieveSnackbar(){
+        val snackbar = Snackbar.make(rootView,"No internet connection",Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("Retrieve") {
+            // Retrieve action: Check internet connectivity again
+            if (isInternetAvailable(context)) {
+                // Internet is available now, perform your action here
+                showSnackbar("Internet is available!")
+            } else {
+                // Internet is still not available, show the retrieve Snackbar again
+                showSnackbar("Still no internet connection!")
+            }
+        }
+        snackbar.show()
+    }
+
+
+    //  internet connectivity check logic
+    private fun isInternetAvailable(context: Context): Boolean {
+        // Get a reference to the ConnectivityManager to check network status.
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        // Retrieve the active network. If it's null, there's no active network connection.
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        // Retrieve network capabilities for the active network.
+        val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        // Check if the active network has the capability of INTERNET, indicating internet access.
+        return actNw.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
 }
+
+
+
+
+
+
+
+
+
+
